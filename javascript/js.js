727fadf24e251972f740c12a16a95b6d264c0a88
Amir Ghareeb <91801875+Amirgharib@users.noreply.github.com>
2023-06-10 20:46:40 +0300

    Add files via upload

---
+++ javascript/js.js
@@ -0,0 +1,112 @@
+document.querySelector(".body1 .re").onclick= function(){
+    
+    document.querySelector("body").style="background-color :red "
+    document.querySelector(".body0 .right .white").style="z-index : 0 "  
+    document.querySelector(".body0 .right .gray").style="z-index : 0 "
+    document.querySelector(".body0 .right .blue").style="z-index : 0 "
+    document.querySelector(".body0 .right .gold").style="z-index : 0 " 
+    document.querySelector(".body0 .right .red").style="z-index : 1 " 
+    }
+    
+    document.querySelector(".body1 .gol").onclick= function(){
+        document.querySelector("body").style="background-color :goldenrod " 
+        document.querySelector(".body0 .right .white").style="z-index : 0 "  
+    document.querySelector(".body0 .right .gray").style="z-index : 0 "
+    document.querySelector(".body0 .right .blue").style="z-index : 0 "
+    document.querySelector(".body0 .right .red").style="z-index : 0 " 
+        document.querySelector(".body0 .right .gold").style="z-index : 1 " 
+        }
+        document.querySelector(".body1 .whit").onclick= function(){
+            document.querySelector("body").style="background-color :#a9a1a1;  " 
+            document.querySelector(".body0 .right .gold").style="z-index : 0 "  
+        document.querySelector(".body0 .right .gray").style="z-index : 0 "
+        document.querySelector(".body0 .right .blue").style="z-index : 0 "
+        document.querySelector(".body0 .right .red").style="z-index : 0 " 
+            document.querySelector(".body0 .right .white").style="z-index : 1 " 
+            }
+            document.querySelector(".body1 .gra").onclick= function(){
+                document.querySelector("body").style="background-color :#312e2e " 
+                document.querySelector(".body0 .right .white").style="z-index : 0 "  
+            document.querySelector(".body0 .right .gold").style="z-index : 0 "
+            document.querySelector(".body0 .right .blue").style="z-index : 0 "
+            document.querySelector(".body0 .right .red").style="z-index : 0 " 
+                document.querySelector(".body0 .right .gray").style="z-index : 1 " 
+                }
+                document.querySelector(".body1 .blu").onclick= function(){
+                    document.querySelector("body").style="background-color :#6c9fa4 " 
+                    document.querySelector(".body0 .right .white").style="z-index : 0 "  
+                document.querySelector(".body0 .right .gray").style="z-index : 0 "
+                document.querySelector(".body0 .right .gold").style="z-index : 0 "
+                document.querySelector(".body0 .right .red").style="z-index : 0 " 
+                    document.querySelector(".body0 .right .blue").style="z-index : 1 " 
+                    }
+                if(screen.availWidth <= "780"){
+                    document.querySelector(".logo img").style=" transform: transform: translate(-9vw, 0px);position: absolute;"
+                    document.querySelector("nav").style=" display : none;"
+                    document.querySelector(".body0 .right .gold").style=" visibility: hidden;"
+                    document.querySelector(".body0 .right .gray").style=" visibility: hidden;"
+                    document.querySelector(".body0 .right .white").style=" visibility: hidden;"
+                    document.querySelector(".body0 .right .blue").style=" visibility: hidden;"
+                    document.querySelector(".body0 .right .red").style=" visibility: hidden;"
+                    window.onload = function(){
+                        document.querySelector(".logo img").style=" transform: translate(-9vw, 0px);"
+                        
+                    document.querySelector(".body0 .right .gold").style=" visibility: visible; transform: translate(-12vw, 72%);  transition: 1s;"
+                    // document.querySelector(".body0 .right .gray").style=" visibility: visible; transform: translate(-12vw, 72%);  transition: 1s;"
+                    // document.querySelector(".body0 .right .white").style=" visibility: visible; transform: translate(-12vw, 72%);  transition: 1s;"
+                    // document.querySelector(".body0 .right .blue").style=" visibility: visible; transform: translate(-12vw, 72%);  transition: 1s;"
+                    // document.querySelector(".body0 .right .red").style=" visibility: visible; transform: translate(-12vw, 72%);  transition: 1s;"
+                    } 
+
+
+                    document.querySelector(".body1 .gol").onclick= function(){
+                        document.querySelector("body").style="background-color :goldenrod " 
+                        document.querySelector(".body0 .right .white").style="z-index : 0;visibility: hidden; "  
+                    document.querySelector(".body0 .right .gray").style="z-index : 0 ;visibility: hidden;"
+                    document.querySelector(".body0 .right .blue").style="z-index : 0 ;visibility: hidden;"
+                    document.querySelector(".body0 .right .red").style="z-index : 0 ;visibility: hidden;" 
+                        document.querySelector(".body0 .right .gold").style="z-index : 1; transform: translate(-12vw, 72%);  transition: 1s; " 
+                        }
+
+
+                        document.querySelector(".body1 .re").onclick= function(){
+    
+                            document.querySelector("body").style="background-color :red "
+                            document.querySelector(".body0 .right .white").style="z-index :0 ;visibility: hidden;"  
+                            document.querySelector(".body0 .right .gray").style="z-index : 0 ;visibility: hidden;"
+                            document.querySelector(".body0 .right .blue").style="z-index : 0 ;visibility: hidden;"
+                            document.querySelector(".body0 .right .gold").style="z-index : 0 ;visibility: hidden;" 
+                            document.querySelector(".body0 .right .red").style="z-index : 1 ;transform: translate(-12vw, 72%);  transition: 1s;" 
+                            }
+
+                    
+                            document.querySelector(".body1 .whit").onclick= function(){
+                                document.querySelector("body").style="background-color :#a9a1a1;  " 
+                                document.querySelector(".body0 .right .gold").style="z-index:0   ;visibility: hidden;"  
+                            document.querySelector(".body0 .right .gray").style="z-index : 0     ;visibility: hidden;"
+                            document.querySelector(".body0 .right .blue").style="z-index : 0     ;visibility: hidden;"
+                            document.querySelector(".body0 .right .red").style="z-index : 0      ;visibility: hidden;" 
+                                document.querySelector(".body0 .right .white").style="z-index : 1; transform: translate(-12vw, 72%);  transition: 1s;" 
+                                }
+
+                                document.querySelector(".body1 .gra").onclick= function(){
+                                    document.querySelector("body").style="background-color :#312e2e " 
+                                    document.querySelector(".body0 .right .white").style="z-index : 0;visibility: hidden;"  
+                                document.querySelector(".body0 .right .gold").style="z-index : 0     ;visibility: hidden;"
+                                document.querySelector(".body0 .right .blue").style="z-index : 0     ;visibility: hidden;"
+                                document.querySelector(".body0 .right .red").style="z-index : 0      ;visibility: hidden;" 
+                                    document.querySelector(".body0 .right .gray").style="z-index : 1 ; transform: translate(-12vw, 72%);  transition: 1s;" 
+                                    }
+                                    document.querySelector(".body1 .blu").onclick= function(){
+                                        document.querySelector("body").style="background-color :#6c9fa4 " 
+                                        document.querySelector(".body0 .right .white").style="z-index : 0   ;visibility: hidden;"  
+                                    document.querySelector(".body0 .right .gray").style="z-index : 0 ;visibility: hidden;"
+                                    document.querySelector(".body0 .right .gold").style="z-index : 0 ;visibility: hidden;"
+                                    document.querySelector(".body0 .right .red").style="z-index : 0  ;visibility: hidden;" 
+                                        document.querySelector(".body0 .right .blue").style="z-index : 1 ; transform: translate(-12vw, 72%);  transition: 1s;" 
+                                        }
+                }
+
+
+
+                
